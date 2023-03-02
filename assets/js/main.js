55c7d084c6f4ca6ca76127be35c12dbc53e43aba
willson-alflen <willson.alflen@gmail.com>
2023-03-02 10:09:37 -0300

    ajusta section#hero, hero-header e section#virtual-reality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,14 @@
 let heroNav = document.querySelector("#hero-navbar");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 700) {
+  if (window.scrollY > 0 && window.scrollY < 720) {
     heroNav.classList.add("bgChangeScroll");
+    heroNav.classList.remove("bgChangeScroll700");
+  } else if (window.scrollY >= 720) {
+    heroNav.classList.remove("bgChangeScroll");
+    heroNav.classList.add("bgChangeScroll700");
   } else {
     heroNav.classList.remove("bgChangeScroll");
+    heroNav.classList.remove("bgChangeScroll700");
   }
 });
